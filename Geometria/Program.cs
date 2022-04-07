eb3402d7cb4e16b9fedad29606afa37f08dc3a6c
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-07 14:58:17 +0200

    Inserito Imput Utente

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Geometria;
+
+//INPUT UTENTE
+Console.WriteLine("Inserici la base del rettangolo: ");
+int baseRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci L'altezza del rettangolo");
+int altezzaRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+//Istanza oggetto
+Rettangolo rettangolo1 = new Rettangolo(baseRettangolo, altezzaRettangolo);
+
+//Display
+Console.WriteLine("L'area del tuo rettangolo è: " + rettangolo1.CalcolaArea());
+Console.WriteLine("Il perimetro del tuo rettangolo è: " + rettangolo1.CalcolaPerimetro());
+
