b75e03d69ca2c3aa1a9279b4e6feffe652af8e02
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-07 16:24:10 +0200

    Migliorata interfaccia utente

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,20 +1,106 @@
 ﻿using Geometria;
 
+//variabli
+int baseRettangolo = 0;
+int altezzaRettangolo = 0;
+
+
+void ChiediRettangolo()
+{
+    Console.WriteLine("Inserici la base del rettangolo in cm: ");
+    bool flag = false;
+    do
+    {
+        if (int.TryParse(Console.ReadLine(), out int output))
+        {
+            baseRettangolo = output;
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Inserisci solo numeri interi!");
+        }
+    } while (!flag);
+    flag = false;
+
+    Console.WriteLine("Inserisci L'altezza del rettangolo in cm: ");
+    do
+    {
+        if (int.TryParse(Console.ReadLine(), out int output))
+        {
+            altezzaRettangolo = output;
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Inserisci solo numeri interi!");
+        }
+    } while (!flag);
+}
+
 //INPUT UTENTE
-Console.WriteLine("Inserici la base del rettangolo in cm: ");
-int baseRettangolo = int.Parse(Console.ReadLine());
-Console.WriteLine("Inserisci L'altezza del rettangolo in cm: ");
-int altezzaRettangolo = int.Parse(Console.ReadLine());
-Console.WriteLine();
+Console.WriteLine("Benventuto nel programma per il calcolo di perimetro ed area di un rettangolo! \n");
+Console.WriteLine("Per prima cosa scegli le dimensioni del tuo rettangolo. \n");
+ChiediRettangolo();
 
 //Istanza oggetto
-Rettangolo rettangolo1 = new Rettangolo(baseRettangolo, altezzaRettangolo);
+Rettangolo rettangolo = new Rettangolo(baseRettangolo, altezzaRettangolo);
 
-//Display
-Console.WriteLine("L'area del tuo rettangolo è: " + rettangolo1.CalcolaArea());
-Console.WriteLine("Il perimetro del tuo rettangolo è: " + rettangolo1.CalcolaPerimetro() + "\n");
+//programma
+while (true)
+{
+    Console.WriteLine("Cosa vuoi fare adesso? (scegli il numero dell'operazione)");
+    Console.WriteLine();
+    Console.WriteLine("[1] -Conoscere l'area del rettangolo");
+    Console.WriteLine("[2] -Conoscere il perimetro del rettangolo");
+    Console.WriteLine("[3] -Stampare tutte le informazioni del rettangolo");
+    Console.WriteLine("[4] -Disegnare il rettangolo");
+    Console.WriteLine("[5] -Cambiare le dimensioni del tuo rettangolo");
+    string scelta = Console.ReadLine();
 
-rettangolo1.StampaRettangolo();
+    switch (scelta)
+    {
+        case "1":
+            Console.WriteLine("L'area del tuo rettangolo è di: " + rettangolo.CalcolaArea() + "cm \n");
+            Console.WriteLine("premi un tasto per andare avanti");
+            Console.ReadKey();
+            break;
+        case "2":
+            Console.WriteLine("Il perimetro del tuo rettangolo è di: " + rettangolo.CalcolaPerimetro() + "cm \n");
+            Console.WriteLine("premi un tasto per andare avanti");
+            Console.ReadKey();
+            break ;
+        case "3":
+            rettangolo.StampaRettangolo();
+            Console.WriteLine("premi un tasto per andare avanti");
+            Console.ReadKey();
+            break;
+        case "4":
+            rettangolo.Disegna();
+            Console.WriteLine("premi un tasto per andare avanti");
+            Console.ReadKey();
+            break;
+        case "5":
+            ChiediRettangolo();
+            Console.WriteLine("premi un tasto per andare avanti");
+            Console.ReadKey();
+            break;
+        default:
+            Console.WriteLine("Seleziona un numero da 1 a 5");
+            break;
+    }
+}
 
-rettangolo1.Disegna();
+
+
+
+
+
+
+
+
+
+
+
+
 
